bca747158ab83db516279be5de61c4445eecfd44
Micaeli Takara Becca <Mikaelitakara@gmail.com>
2023-08-25 20:32:45 -0300

    FEAT: estilização finalizada LoginPage e SignupPage

--- src/pages/Signup/SignupPage.jsx
+++ src/pages/Signup/SignupPage.jsx
@@ -1,7 +1,65 @@
+import { useState } from "react";
+import {
+  ContainerAutorization,
+  Form,
+  Main,
+  TitleSignup,
+  ButtonCreateCount,
+  Heading,
+} from "./SignupPageStyle";
+
 export default function SignupPage() {
-    return(
-        <>
-        <p>SIGNUP</p>
-        </>
-    )
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <Main>
+      <div>
+        <TitleSignup>
+          <strong>
+            Olá, boas vindas ao <Heading>LabEddit!</Heading>
+          </strong>
+        </TitleSignup>
+      </div>
+      <Form>
+        <div className="input-container">
+          <input type="name" value={name} onChange={handleNameChange} />
+          <label className={name ? "active" : ""}>Apelido</label>
+        </div>
+        <div className="input-container">
+          <input type="email" value={email} onChange={handleEmailChange} />
+          <label className={email ? "active" : ""}>E-mail</label>
+        </div>
+        <div className="input-container">
+          <input type="password" value={password} onChange={handlePasswordChange} />
+          <label className={password ? "active" : ""}>Senha</label>
+        </div>
+      </Form>
+      <ContainerAutorization>
+        <p className="TextAuthorization">
+          Ao continuar, você concorda com o nosso
+          <span> Contrato de usuário </span> e nossa
+          <span> Política de Privacidade </span>
+        </p>
+        <div className="ContainerCheckbox">
+          <input type="checkbox" />
+          <p>Eu concordo em receber emails sobre coisas legais no Labeddit</p>
+        </div>
+      </ContainerAutorization>
+      <ButtonCreateCount>Cadastrar</ButtonCreateCount>
+    </Main>
+  );
 }
