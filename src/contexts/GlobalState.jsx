dd9ffc0eb4d51fdf5e6c446c1d5f86e5e75ecd7e
Micaeli Takara Becca <Mikaelitakara@gmail.com>
2023-09-02 21:32:31 -0300

    FEAT: likes e dislikes das postagens funcionando

--- src/contexts/GlobalState.jsx
+++ src/contexts/GlobalState.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+// GlobalStates.js
+import React, { useState } from "react";
 import { GlobalContext } from "./GlobalContext";
 import useForm from "../hooks/useForm";
-import { BASE_URL } from "../constants/url";
-import axios from "axios";
 
 export const GlobalStates = (props) => {
   const [posts, setPosts] = useState([]);
@@ -10,48 +9,23 @@ export const GlobalStates = (props) => {
     content: "",
   });
 
-  const getPosts = async () => {
-    try {
-      const response = await axios.get(`${BASE_URL}/posts/`, {
-        headers: {
-          Authorization: window.localStorage.getItem("token"),
-        },
-      });
-      setPosts(response.data);
-    } catch (error) {
-      console.log(error.response);
-    }
+  const updateLikes = (postId, newLikes) => {
+    const updatedPosts = posts.map((post) => {
+      if (post.id === postId) {
+        return { ...post, likes: newLikes };
+      }
+      return post;
+    });
+    setPosts(updatedPosts);
   };
 
-  const addNewPost = async () => {
-    try {
-      const body = {
-        content: form.content,
-      };
-      const response = await axios.post(`${BASE_URL}/posts`, body, {
-        headers: {
-          Authorization: window.localStorage.getItem("token"),
-        },
-      });
-      window.localStorage.setItem("token", response.data.output.token);
-      getPosts();
-      cleanForm();
-    } catch (error) {
-      console.log(error.response);
-    }
-  };
-  
-  useEffect(() => {
-    getPosts();
-  }, []);
-  
   const context = {
     posts,
     setPosts,
     form,
     onChange,
     cleanForm,
-    addNewPost
+    updateLikes
   };
 
   return (
